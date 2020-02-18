cea9cea3dabd3939d3517a5645f6e3271e2e2678
Zack McKenna <zackmckenna@gmail.com>
2020-02-18 15:13:24 -0500

    added axios

--- redux/climbs.js
+++ redux/climbs.js
@@ -1,16 +1,27 @@
 import { baseUrl } from '../constants/BaseUrl'
+import { tokenToString } from 'typescript';
 
 export const GET_CLIMBS = 'bould/redux/climbs/GET_CLIMBS';
 export const GET_CLIMBS_SUCCESS = 'bould/redux/climbs/GET_CLIMBS_SUCCESS';
 export const GET_CLIMBS_FAIL = 'bould/redux/climbs/GET_CLIMBS_FAIL';
 export const GET_CLIMBS_PENDING = 'bould/redux/climbs/GET_CLIMBS_PENDING';
 
-export const ADD_CLIMB_SUCCESS = 'bould/redux/climbs/ADD_CLIMB_SUCCESS';
-export const ADD_CLIMB_FAIL = 'bould/redux/climbs/ADD_CLIMB_FAIL';
-export const ADD_CLIMB_PENDING = 'bould/redux/climbs/ADD_CLIMB_FAIL';
+export const POST_CLIMB_SUCCESS = 'bould/redux/climbs/POST_CLIMB_SUCCESS';
+export const POST_CLIMB_FAIL = 'bould/redux/climbs/POST_CLIMB_FAIL';
+export const POST_CLIMB_PENDING = 'bould/redux/climbs/POST_CLIMB_PENDING';
 
 export default function reducer(state = { climbs: [] }, action) {
   switch (action.type) {
+    case POST_CLIMB_PENDING:
+      return { ...state, postClimbPending: true };
+    case POST_CLIMB_SUCCESS:
+      return { ...state, postClimbPending: false };
+    case POST_CLIMB_FAIL:
+      return {
+        ...state,
+        postClimbPending: false,
+        error: 'Error posting'
+      };
     case GET_CLIMBS:
       return { ...state, loading: true };
     case GET_CLIMBS_SUCCESS:
@@ -25,6 +36,51 @@ export default function reducer(state = { climbs: [] }, action) {
       return state;
   }
 }
+
+export const postClimbPending = () => {
+  return {
+    type: POST_CLIMB_PENDING
+  }
+}
+
+export const postClimbSuccess = climb => {
+  return {
+    type: POST_CLIMB_SUCCESS,
+    payload: climb
+  }
+}
+
+export const postClimbFail = error => {
+  return {
+    type: POST_CLIMB_FAIL,
+    error: error
+  }
+}
+
+export const postClimb = () => dispatch => {
+  dispatch(postClimbPending())
+  return fetch(baseUrl + 'api/climbs', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      personalDifficulty: personalDifficulty,
+      setDifficulty: setDifficulty,
+      result: result,
+      note: note,
+      completed: completed,
+      token: token
+    }),
+  })
+  .then(response => response.json())
+  .then(climb => {
+    dispatch(postClimbSuccess(climb))
+  })
+  .catch(error => dispatch(postClimbError(error)))
+}
+
 
 export const getClimbsPending = () => {
   return {
